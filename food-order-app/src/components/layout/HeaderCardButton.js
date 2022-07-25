d04d42fa94a6e0555297972b8ea057c708ef32ec
Giovane Aires Gomes <giovaneag17@outlook.com>
2022-07-25 11:35:39 -0300

    feat: adding animation to cart items changes

--- food-order-app/src/components/layout/HeaderCardButton.js
+++ food-order-app/src/components/layout/HeaderCardButton.js
@@ -1,19 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../cart/CartIcon";
 import styles from "./HeaderCardButton.module.css";
 
 const HeaderCardButton = ({ onClick }) => {
   const cartContext = useContext(CartContext);
+  const { items } = cartContext;
+  const [buttonIsHighLighted, setButtonIsHighLighted] = useState(false);
 
-  const totalOfCartItems = cartContext.items.reduce(
+  const totalOfCartItems = items.reduce(
     (currentResult, item) => currentResult + item.amount,
     0,
   );
 
+  const buttonClasses = `${styles.button} ${buttonIsHighLighted ? styles.bump: ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+
+    setButtonIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighLighted(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
     <button
-      className={styles.button}
+      className={buttonClasses}
       onClick={onClick}
     >
       <span className={styles.icon}>
