63b2eba422b52ccd8823ffd4b5788814cda9c677
Giovane Aires Gomes <giovaneag17@outlook.com>
2022-07-25 09:55:01 -0300

    feat: added cart item logic working with refs and forwardRefs

--- food-order-app/src/components/meals/MealItem/MealItem.js
+++ food-order-app/src/components/meals/MealItem/MealItem.js
@@ -1,8 +1,20 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import styles from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
 
 const MealItem = ({ id, name, description, price }) => {
+  const cartContext = useContext(CartContext);
   const formatedPrice = `$${price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartContext.addItem({
+      id,
+      name,
+      amount,
+      price,
+    })
+  }
 
   return (
     <li className={styles.meal}>
@@ -12,7 +24,7 @@ const MealItem = ({ id, name, description, price }) => {
         <div className={styles.price}>{formatedPrice}</div>
       </div>
       <div>
-        <MealItemForm id={id}/>
+        <MealItemForm id={id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
--- food-order-app/src/components/meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import styles from './MealItemForm.module.css';
 
-const MealItemForm = ({ id }) => {
+const MealItemForm = ({ id, onAddToCart }) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;  
+    }
+
+    onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: `amount_${id}`,
@@ -16,6 +39,7 @@ const MealItemForm = ({ id }) => {
         }}
       />
       <button >+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   )
 };
